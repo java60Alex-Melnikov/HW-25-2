dd8f492285dc35edb0e0d5a16692f5cbd9633242
Millenium-Konig <sanyamelnikov097+@gmail.com>
2025-10-18 18:40:45 +0300

    added push and pop methods

--- main.py
+++ main.py
@@ -2,3 +2,16 @@ class MyStackInt:
     def __init__(self):
         self._stack = []
         self._max_stack = []
+
+    def push(self, num: int):
+        self._stack.append(num)
+        if not self._max_stack or num >= self._max_stack[-1]:
+            self._max_stack.append(num)
+        else:
+            self._max_stack.append(self._max_stack[-1])
+    
+    def pop(self) -> int:
+        if not self._stack:
+            raise IndexError("pop from empty stack")
+        self._max_stack.pop()
+        return self._stack.pop()
