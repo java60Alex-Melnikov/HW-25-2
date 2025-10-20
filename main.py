f84bcfc55d45e309f24c5957193f4b20feaadb2f
Millenium-Konig <sanyamelnikov097+@gmail.com>
2025-10-20 19:56:49 +0300

    fixed the performance issue

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ class MyStackInt:
         self._stack.append(num)
         if not self._max_stack or num >= self._max_stack[-1]:
             self._max_stack.append(num)
-        else:
-            self._max_stack.append(self._max_stack[-1])
     
     def pop(self) -> int:
         if not self._stack:
             raise IndexError("pop from empty stack")
-        self._max_stack.pop()
-        return self._stack.pop()
+        num = self._stack.pop()
+        if num == self._max_stack[-1]:
+            self._max_stack.pop()
+        return num
     
     def max(self) -> int:
         if not self._max_stack:
